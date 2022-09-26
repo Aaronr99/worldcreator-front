dbaf8536b4de429dcbe1828f4dc0ee92ea22b301
Aaronr99 <38481944+Aaronr99@users.noreply.github.com>
2022-09-26 18:38:46 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -160,16 +160,17 @@ function App() {
                 }
               }} />
             </div>
-            <select
-              className="form-select" id='parent-selection'
-              value={toEdit} onChange={(e) => {
-                setToEdit(e.target.value)
-                loadOrUnloadValues(e.target.value)
-              }}>
-              <option value=''>no one to edit</option>
-              {allElements.map(x => <option key={x._id} value={x._id}> {x.title}  </option>)}
-            </select>
           </div>
+
+          <select
+            className="form-select" id='parent-selection'
+            value={toEdit} onChange={(e) => {
+              setToEdit(e.target.value)
+              loadOrUnloadValues(e.target.value)
+            }}>
+            <option value=''>no one to edit</option>
+            {allElements.map(x => <option key={x._id} value={x._id}> {x.title}  </option>)}
+          </select>
 
           <select
             className="form-select" id='parent-selection'
